d27ba3798e05f6c87b04d1550722a00bdecdfe5c
franilones <franolmosdev@gmail.com>
2020-07-14 16:20:27 +0200

    Controler to add, delete show and modified the database

---
+++ Controllers/AuthorsController.cs
@@ -0,0 +1,78 @@
+﻿using LibraryDemoApi.Context;
+using LibraryDemoApi.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryDemoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ApplicationDBContext context;
+        //Dependency injection 
+        public AuthorsController(ApplicationDBContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Author>> Get()
+        {
+            return context.Authors.ToList();
+        }
+
+        [HttpGet("{id}", Name = "ObtainResource")]
+        public ActionResult<Author> Get(int id)
+        {
+            var author = context.Authors.FirstOrDefault(x => x.Id == id);
+
+            if(null == author)
+            {
+                return NotFound();
+            }
+            return author;
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Author author)
+        {
+            context.Authors.Add(author);
+            context.SaveChanges();
+            //If everything went well it will call the route ObtainSource that we declared before
+            return new CreatedAtRouteResult("ObtainResource", new { id = author.Id}, author);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Author value)
+        {
+            if(id != value.Id)
+            {
+                return BadRequest();
+            }
+
+            context.Entry(value).State = EntityState.Modified;
+            context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<Author> Delete(int id)
+        {
+            var author = context.Authors.FirstOrDefault(x => x.Id == id);
+
+            if(null == author)
+            {
+                return NotFound();
+            }
+
+            context.Authors.Remove(author);
+            context.SaveChanges();
+            return author;
+        }
+    }
+}
